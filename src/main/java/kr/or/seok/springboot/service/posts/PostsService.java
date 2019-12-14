4ffaaf9c0181c33a8f5f4575de494e1d09608733
SeokRae <kslbsh@gmail.com>
2019-12-15 00:17:19 +0900

    p.145 all posting view

--- src/main/java/kr/or/seok/springboot/service/posts/PostsService.java
+++ src/main/java/kr/or/seok/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package kr.or.seok.springboot.service.posts;
 
 import kr.or.seok.springboot.domain.posts.Posts;
 import kr.or.seok.springboot.domain.posts.PostsRepository;
+import kr.or.seok.springboot.web.dto.PostsListResponseDto;
 import kr.or.seok.springboot.web.dto.PostsResponseDto;
 import kr.or.seok.springboot.web.dto.PostsSaveRequestDto;
 import kr.or.seok.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 존재하지 않습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
