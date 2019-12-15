4c91b9246be9d7493f79b3b3aa6508a218a11166
SeokRae <kslbsh@gmail.com>
2019-12-15 17:53:50 +0900

    p.176  google login setting  spring security setting  login test  user author posting test

--- src/main/java/kr/or/seok/springboot/web/IndexController.java
+++ src/main/java/kr/or/seok/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package kr.or.seok.springboot.web;
 
+import kr.or.seok.springboot.config.auth.dto.SessionUser;
 import kr.or.seok.springboot.service.posts.PostsService;
 import kr.or.seok.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user !=  null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -29,7 +38,8 @@ public class IndexController {
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
-
         return "posts-update";
     }
+
+
 }
